c935b3cb922563ebb4c236b0fad757cd6eb0f56b
Julio Cesar <julio.csjunior@b2wdigital.com>
2022-10-14 14:25:08 -0300

    Criação de tabela usuarios

--- ongoingfront/models.py
+++ ongoingfront/models.py
@@ -1,6 +1,11 @@
+from pyexpat import model
 from unicodedata import name
 from django.db import models
 
-# Create your models here.
-# class Usuario(models.Model):
-#     login = models.CharField(max_length=100)
+class Usuario(models.Model):
+    nome = models.CharField(max_length = 100)
+    cargo = models.CharField(max_length = 100)
+    status = models.CharField(max_length = 100)
+    turno = models.CharField(max_length = 100)
+    email = models.CharField(max_length = 254)
+    senha = models.CharField(max_length = 100)
