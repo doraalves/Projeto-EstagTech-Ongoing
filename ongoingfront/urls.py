61c1ff634c8101b847177efca457e5ddc270b5b4
Izadora Alves <izadora.alves@americanas.io>
2022-10-20 14:44:50 -0300

    Atualizando o botão de logout

--- ongoingfront/urls.py
+++ ongoingfront/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.ver_login, name="ver_login"),
     path('painel/', views.ver_painel, name="ver_painel"),
-    path('cadastro/', views.ver_cadastro, name="ver_cadastro")
+    path('cadastro/', views.ver_cadastro, name="ver_cadastro"),
+    path('logout/', auth_views.LogoutView.as_view(), name="logout")
 ]
